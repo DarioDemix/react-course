1f711abd83edf5fc8c5d981b58969274b5d8ac1b
Dario De Marco <dario.demarco@mywan.it>
2021-07-09 12:06:34 +0200

    Moved the initial expenses in a JSON file; made the expense list dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,36 +5,24 @@ import "./Expenses.css";
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const expenses = props.expenses;
-  const [selectedYear, setSelectedYear] = useState('2021');
+  const [selectedYear, setSelectedYear] = useState("2021");
 
-  const selectionHandler = year => setSelectedYear(year);
+  const selectionHandler = (year) => setSelectedYear(year);
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={selectedYear} onSelection={selectionHandler}/>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <ExpenseFilter selected={selectedYear} onSelection={selectionHandler} />
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
     </Card>
   );
-}
+};
 
 export default Expenses;

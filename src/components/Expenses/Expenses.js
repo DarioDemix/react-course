196cf2660f44f21645cf2174bfbe1cb315421b6f
Dario De Marco <dario.demarco@mywan.it>
2021-07-10 09:51:03 +0200

    Implemented filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,12 +8,14 @@ const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2021");
 
   const selectionHandler = (year) => setSelectedYear(year);
-  const expenses = props.expenses.filter(exp =>  exp.date.getFullYear() === +selectedYear);
+  const expenses = props.expenses.filter(
+    (exp) => exp.date.getFullYear() === +selectedYear
+  );
 
-  return (
-    <Card className="expenses">
-      <ExpenseFilter selected={selectedYear} onSelection={selectionHandler} />
-      {expenses.map((expense) => {
+  const evaluateExpensesContent = () => {
+    let expensesContent = <p>No expenses found.</p>;
+    if (expenses.length > 0) {
+      expensesContent = expenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
@@ -22,7 +24,15 @@ const Expenses = (props) => {
             date={expense.date}
           />
         );
-      })}
+      });
+      return expensesContent;
+    }
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter selected={selectedYear} onSelection={selectionHandler} />
+      {evaluateExpensesContent()}
     </Card>
   );
 };

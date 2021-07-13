36f77bf5eccb1381b0074336f9d88feccc37c7f9
Dario De Marco <dario.demarco@mywan.it>
2021-07-13 11:58:35 +0200

    Created ExpensesList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,36 +3,19 @@ import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2021");
 
-  const selectionHandler = (year) => setSelectedYear(year);
   const expenses = props.expenses.filter(
     (exp) => exp.date.getFullYear() === +selectedYear
   );
 
-  const evaluateExpensesContent = () => {
-    let expensesContent = <p>No expenses found.</p>;
-    if (expenses.length > 0) {
-      expensesContent = expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      });
-      return expensesContent;
-    }
-  };
-
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={selectedYear} onSelection={selectionHandler} />
-      {evaluateExpensesContent()}
+      <ExpenseFilter selected={selectedYear} onSelection={setSelectedYear} />
+      <ExpensesList expenses={expenses}></ExpensesList>
     </Card>
   );
 };

1f205bb646a72e8a23dbd64be1f7aee34fab82e5
Dario De Marco <dario.demarco@mywan.it>
2021-07-16 15:51:04 +0200

    setup module project

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,15 @@
-import styles from "./Button.module.css";
+import React from 'react';
 
-const Button = props => {
+import classes from './Button.module.css';
+
+const Button = (props) => {
   return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
+    <button
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );

b33df542dd2b3d0a2129579f65e7d808c8c26534
Dario De Marco <dario.demarco@mywan.it>
2021-07-16 17:13:20 +0200

    Added cleanup function to useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+
+    const timeout = setTimeout(() => {
+      console.log(`Check form validity!`)
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log(`cleanup`);
+      clearTimeout(timeout);
+    }; // cleanup function
 
   }, [enteredEmail, enteredPassword]);
 

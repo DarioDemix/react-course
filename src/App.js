4354a4619139f8849b536cfdecd61f4f59d66c12
Dario De Marco <dario.demarco@mywan.it>
2021-07-19 14:22:18 +0200

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./components/context/auth-context";
-import React, { useContext } from "react";
-
 function App() {
-  const cxt = useContext(AuthContext);
-
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!cxt.isLoggedIn && <Login onLogin={cxt.loginHandler} />}
-        {cxt.isLoggedIn && <Home onLogout={cxt.logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
